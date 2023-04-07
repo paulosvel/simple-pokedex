173410ad9667c0340931fc7ad9ef8071c8161366
paulosvel <paul2001vel@gmail.com>
2023-04-07 14:35:19 +0300

    added pokemon names and imgs

--- src/App.js
+++ src/App.js
@@ -2,36 +2,39 @@ import { Box, Button } from "@mui/material";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Typography from "@mui/material";
 
 function App() {
-async function getPikachu(){
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
-    setPokemon(response);
-    console.log(response);
-  }-
-  useEffect (() => {
-  getPikachu();
-  }
-  );
   const [pokemon, setPokemon] = useState([]);
-
-
+  async function getPikachu() {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+    setPokemon(response.data.results);
+    console.log(response.data.results);
+  }
+  useEffect(() => {
+    getPikachu();
+  });
 
   return (
     <>
-      {pokemon&&
-      pokemon.map((item)=>{
-        item.data.map((item)=>{
-          return(
-            <Box>
-              {item.name}
-            </Box>
-          )
-        })
-      })}
+      {pokemon &&
+        pokemon.map((item, i) => {
+          return (
+            
+            <>
+              <Box>{item.name}</Box>
+
+              <Box>
+                {" "}
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`}
+                ></img>
+              </Box>
+            </>
+          );
+        })}
     </>
   );
-
 }
 
 export default App;
